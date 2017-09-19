d89232b488677fe04b9496ffae529c6f4cee2b58
Owen Pattison <opattison@neworbit.co.uk>
2017-09-19 21:41:02 +0100

    adding repo

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import './App.css';
+
+interface SquareProps {
+  value: number;
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Game />
+    );
+  }
+}
+
+class Square extends React.Component<SquareProps, {}> {
+  constructor() {
+    super();
+
+    this.state = {
+      value: null
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i: number) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
